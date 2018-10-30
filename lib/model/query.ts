f010d850352aec0ec9695afe6c824bb6e4e51f7b
bglee <deptno@gmail.com>
2018-10-30 22:08:26 +0900

    - query.range interface

--- lib/model/query.ts
+++ lib/model/query.ts
@@ -1,15 +1,15 @@
 import {DocumentClient} from 'aws-sdk/lib/dynamodb/document_client'
+import {always, cond, is, mergeWith, Omit, T} from 'ramda'
 import {$between, $eq, $ge, $gt, $le, $lt, $ne} from '../operator/expression/comparison-operator'
-import {mergeOp, replacementIdGenerator} from '../operator/expression/helper'
+import {replacementIdGenerator} from '../operator/expression/helper'
 import {TScalar} from '../operator/operator'
 import {getLogger} from '../util/debug'
-import {Omit, always, join, head, tap, omit, mergeWith, cond, is, T, concat} from 'ramda'
 
 type DxQueryInput = Omit<DocumentClient.QueryInput, 'TableName'> & {
   __pages?: number
 }
 
-class HashQuery<S> implements Query<S> {
+export class HashQuery<S, H extends TScalar> implements Query<S> {
   params: DxQueryInput = {
     KeyConditionExpression   : `#HSK = :HSK`,
     ExpressionAttributeNames : {
@@ -21,7 +21,7 @@ class HashQuery<S> implements Query<S> {
   } as DxQueryInput
   protected replacementIdGenerator = replacementIdGenerator()
 
-  constructor(private runner, private operator, private hashKeyName, private hashKey) {
+  constructor(private runner, private operator, private hashKeyName, private hashKey: H) {
   }
 
   project(...operators) {
@@ -89,15 +89,9 @@ class HashQuery<S> implements Query<S> {
     )
   }
 }
-class CompositeQuery<S, R extends TScalar> extends HashQuery<S> implements Query<S> {
-  constructor(runner, operator, hashKeyName, hashKey, private rangeKeyName) {
-    super(runner, operator, hashKeyName, hashKey)
-  }
-
-  range()
-  range(value: R): this
-  range(value?: R) {
-    const withRangeKey = (rangeKey: R | null, params) => {
+export class CompositeQuery<S, H extends TScalar, R extends TScalar> extends HashQuery<S, H> implements Query<S> {
+  range(rangeKeyName: keyof S) {
+    const withRangeKey = (rangeKey: R|null, params) => {
       this.merge(params)
       if (rangeKey !== null) {
         this.merge({
@@ -108,13 +102,13 @@ class CompositeQuery<S, R extends TScalar> extends HashQuery<S> implements Query
       }
       this.merge({
         ExpressionAttributeNames: {
-          '#RGK': this.rangeKeyName,
+          '#RGK': rangeKeyName as string
         },
       })
       return this
     }
     const rangeKeyReplacer = always(':RGK')
-    const operators = {
+    return {
       eq     : (value: R) => {
         return withRangeKey(value, $eq(value)(rangeKeyReplacer))
       },
@@ -137,25 +131,13 @@ class CompositeQuery<S, R extends TScalar> extends HashQuery<S> implements Query
         return withRangeKey(null, $between(a, b)(this.replacementIdGenerator))
       }
     }
-    if (value !== undefined) {
-      return operators.eq(value)
-    }
-    return operators
   }
 }
 
-export function createQuery<S, H extends TScalar, R extends TScalar>(runner, operator, hashKeyName: string, hashKey: H): HashQuery<S>
-export function createQuery<S, H extends TScalar, R extends TScalar>(runner, operator, hashKeyName: string, hashKey: H, rangeKeyName: string): CompositeQuery<S, R>
-export function createQuery(runner, operator, hashKeyName, hashKey, rangeKeyName?) {
-  if (rangeKeyName) {
-    return new CompositeQuery(runner, operator, hashKeyName, hashKey, rangeKeyName)
-  }
-  return new HashQuery(runner, operator, hashKeyName, hashKey)
-}
 const logger = getLogger(__filename)
 
 interface Query<S> {
-  //  range?(...operators: Operator[]): this
+//  range?(...operators: Operator[]): this
   project(...operators: Operator[]): this
   filter(...operators: Operator[]): this
   limit(limit: number): this
